7560eca82370c2412448fbae72af5d326e532b5e
James Wu <thefreeasian@gmail.com>
2020-05-30 16:59:56 -0700

    add sorting

--- src/features/sales/TablePanel.js
+++ src/features/sales/TablePanel.js
@@ -1,32 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './TablePanel.module.css';
 
+const SORT_TYPES = {
+  WEEK: 'week',
+  RETAIL: 'retail',
+  WHOLESALE: 'wholesale',
+  UNITS: 'units',
+  RETAILER: 'retailer',
+};
+
 function TablePanel({ sales }) {
+  const [sortType, setSortType] = useState(SORT_TYPES.WEEK);
+
   if (!sales) {
     return null;
   }
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+  });
+  const formatPrice = (price) => {
+    return formatter.format(price);
+  }
+
+  const sortedSales = [...sales].sort((a, b) => {
+    switch(sortType) {
+      case SORT_TYPES.WEEK:
+        return a.weekEnding > b.weekEnding ? -1 : 1;
+      case SORT_TYPES.RETAIL:
+        return a.retailSales > b.retailSales ? -1 : 1;
+      case SORT_TYPES.WHOLESALE:
+        return a.wholesaleSales > b.wholesaleSales ? -1 : 1;
+      case SORT_TYPES.UNITS:
+        return a.unitsSold > b.unitsSold ? -1 : 1;
+      case SORT_TYPES.RETAILER:
+        return a.retailerMargin > b.retailerMargin ? -1 : 1;
+      default:
+        return a.weekEnding < b.weekEnding ? -1 : 1;
+    };
+  });
+
+  const switchSort = (sort) => {
+    return setSortType.bind(null, sort);
+  };
 
   return (
     <div className={styles.tablePanel}>
       <table>
         <thead>
           <tr>
-            <th>WEEK ENDING</th>
-            <th>RETAIL SALES</th>
-            <th>WHOLESALE SALES</th>
-            <th>UNITS SOLD</th>
-            <th>RETAILER MARGIN</th>
+            <th onClick={switchSort(SORT_TYPES.WEEK)}>WEEK ENDING</th>
+            <th onClick={switchSort(SORT_TYPES.RETAIL)}>RETAIL SALES</th>
+            <th onClick={switchSort(SORT_TYPES.WHOLESALE)}>WHOLESALE SALES</th>
+            <th onClick={switchSort(SORT_TYPES.UNITS)}>UNITS SOLD</th>
+            <th onClick={switchSort(SORT_TYPES.RETAILER)}>RETAILER MARGIN</th>
           </tr>
         </thead>
         <tbody>
-          {sales.map(({ weekEnding, retailSales, wholesaleSales, unitsSold, retailerMargin }) => {
+          {sortedSales.map(({ weekEnding, retailSales, wholesaleSales, unitsSold, retailerMargin }) => {
             return (
               <tr>
                 <td>{weekEnding}</td>
-                <td>${retailSales}</td>
-                <td>${wholesaleSales}</td>
+                <td>{formatPrice(retailSales)}</td>
+                <td>{formatPrice(wholesaleSales)}</td>
                 <td>{unitsSold}</td>
-                <td>${retailerMargin}</td>
+                <td>{formatPrice(retailerMargin)}</td>
               </tr>
             );
           })}
