f2881fb951ac08bb97ea752bd6f4847f29a310f8
James Wu <thefreeasian@gmail.com>
2020-05-30 17:03:29 -0700

    tighten up styling

--- src/features/sales/SalesCharts.js
+++ src/features/sales/SalesCharts.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectSales } from './salesSlice';
 import TablePanel from './TablePanel';
+import styles from './SalesCharts.module.css';
 
 function SalesCharts() {
   const sales = useSelector(selectSales);
   return (
-    <div className="salesCharts">
+    <div className={styles.salesCharts}>
       <TablePanel sales={sales} />
     </div>
   );
